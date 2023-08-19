1d6a668009d8298bec071a66072e254d1576a0af
Kuba <kuba>
2023-08-19 18:31:56 +0200

    pull requests

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-           CHange to feature
+           pull request
         </p>
         <a
           className="App-link"
